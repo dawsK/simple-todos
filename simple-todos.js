e9889fa7567515eed2adbbbe100323f278a3f91a
dawsK <dkroeker@gmail.com>
2015-07-28 13:53:14 -0500

    Add checkboxes and delete buttons

--- simple-todos.js
+++ simple-todos.js
@@ -24,6 +24,18 @@ if (Meteor.isClient) {
       });
 
       event.target.text.value = "";
+    },
+
+    "click .toggle-checked": function () {
+      Tasks.update(this._id, {
+        $set: {
+          checked: !this.checked
+        }
+      });
+    },
+    
+    "click .delete": function () {
+      Tasks.remove(this._id);
     }
   });
 
