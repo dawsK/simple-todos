c6053097c76244e63e25e4bc11cdb67d745b85a0
dawsK <dkroeker@gmail.com>
2015-07-29 15:23:27 -0500

    Add hide completed feature

--- simple-todos.js
+++ simple-todos.js
@@ -4,11 +4,18 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({}, {
-        sort: {
-          createdAt: -1
-        }
-      });
+      if (Session.get("hideCompleted")) {
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        return Tasks.find({}, {
+          sort: {
+            createdAt: -1
+          }
+        });
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
     }
   });
 
@@ -24,6 +31,10 @@ if (Meteor.isClient) {
       });
 
       event.target.text.value = "";
+    },
+    
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     },
 
     "click .toggle-checked": function () {
