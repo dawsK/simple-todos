d965b8112ff9717d20e108edcb6f9924146c93b1
dawsK <dkroeker@gmail.com>
2015-07-29 16:04:48 -0500

    Add user authentication

--- simple-todos.js
+++ simple-todos.js
@@ -30,7 +30,9 @@ if (Meteor.isClient) {
 
       Tasks.insert({
         text: text,
-        createdAt: new Date()
+        createdAt: new Date(),
+        owner: Meteor.userId(),
+        username: Meteor.user().username
       });
 
       event.target.text.value = "";
@@ -52,5 +54,8 @@ if (Meteor.isClient) {
       Tasks.remove(this._id);
     }
   });
-
+  
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
 }
